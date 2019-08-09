f45252d47b9fa0393870c717a0a2793443f9f75f
Steve Tueno <steve.jeffrey.tueno.fotso@usherbrooke.ca>
2019-08-09 09:32:15 +0200

    Optimisation of Multidimensional classification algorithm speed

--- multi_dimension_from_file/MultiLinearizationClassification.scala
+++ multi_dimension_from_file/MultiLinearizationClassification.scala
@@ -1,5 +1,6 @@
 package multi_dimension_from_file
 
+import org.apache.spark.sql.types.DoubleType
 import org.apache.spark.sql.{Dataset, Row, SparkSession}
 
 /*
@@ -21,7 +22,7 @@ object MultiLinearizationClassification {
     //processLinearizationClassification("/Users/steve/Downloads/sample_binary_classification_data.txt", 0.6)
     /*Classification error = 0.027777777777777776 well classified count = 35/36 for sample_binary_classification_data.txt M=692 */
     processLinearizationClassification("/Users/steve/Downloads/a1a.txt", 0.6)
-    /*Classification error = 0.027777777777777776 well classified count = 35/36 for a1a.txt M=119 */
+    /* Classification error = 0.2697594501718213 well classified count = 425/582 for a1a.txt M=119 K=10 */
 
 
   }
@@ -36,6 +37,8 @@ object MultiLinearizationClassification {
       .appName("Time Usage")
       .config("spark.master", "local")
       .getOrCreate()
+  spark.sparkContext.setLogLevel("ERROR")
+  import org.apache.spark.sql.functions._
   import spark.implicits._
   def processLinearizationClassification(filePath:String, percentage: Double) = {
 
@@ -64,39 +67,57 @@ object MultiLinearizationClassification {
 
     //XTest.take(50).foreach(head=>System.out.println("a row of XTest : "+head.mkString(", ")+" & "+head.getAs[org.apache.spark.ml.linalg.SparseVector](1).indices.mkString(", ")+" "+head.getAs[org.apache.spark.ml.linalg.SparseVector](1).values.mkString(", ")))
 
-    val Yprime = Util.prime(X, Map[Row, Double]())
+    val Yprime = X.map(row=>YPrime(1, row.getDouble(0), Util.prime(row))).cache()
 
-
-    //overview of predictions
-    X.take(20).map(row => (row.mkString(","), predict(row, Yprime))).foreach(t => println("x = " + t._1  +  " et ypre = " + t._2))
 
 
     //X.indices.foreach(i=>println(Y(i)+ " "+ X(i).indices.map(j => (j + 1) + ":" + X(i)(j)).mkString(" ")))
     //XTest.indices.foreach(i=>println(YTest(i)+ " "+ XTest(i).indices.map(j => (j + 1) + ":" + XTest(i)(j)).mkString(" ")))
 
 
+    //overview of predictions
+    X.take(20).map(row => (row.mkString(","), predict(row, Yprime))).foreach(t => println("x = " + t._1  +  " et ypre = " + t._2))
 
 
-    val wellClassifiedCount = XTest.filter(r=> r.getDouble(0) == predict(r, Yprime)).count()
+
+    val wellClassifiedCount = XTest.collect().par.count(r => predict(r, Yprime) == r.getDouble(0))
     val countXtest = XTest.count()
+
 
     println("Classification error = " + (countXtest - wellClassifiedCount*1.0) / countXtest + " well classified count = " + wellClassifiedCount + "/" + countXtest)
 
+
+
+
   }
 
-  def predict(x: Row,  Yprime: Map[Row, Double]): Double = {
+  def predict(x: Row,  Yprime: Dataset[YPrime]): Double = {
 
     val yprime = Util.prime(x)
 
+    //println("Yprime: "+Yprime)
+    //println("Yprime colums: "+ Yprime.take(20).mkString(", "))
 
+    val topYPrimeIndices = Yprime.orderBy(udf((x:Double)=>Math.abs(yprime-x), DoubleType)($"yprime").asc).limit(Util.k)
+
+
+    val res = topYPrimeIndices.reduce((r1, r2)=>YPrime(r1.count+r2.count, 0.0, r1.getYPrime(yprime) + r2.getYPrime(yprime)))
+
+    if (res.yprime/ res.count >= 0.5) 1 else 0
+
+    //println(" topYPrimeIndices "+ topYPrimeIndices)
 
     //k-nearest neighbors
-    val topYPrimeIndices = Util.top(Yprime.toList.map(Util.Coord), Util.k)(new Util.CoordOrdering(Util.Coord((x, yprime)))).map(_.x._1)
+    //val topYPrimeIndices = Util.top(Yprime.map(Util.Coord), Util.k)(new Util.CoordOrdering(Util.Coord((x, yprime)))).map(_.x._1)
 
-    if (topYPrimeIndices.aggregate(0.0)((s, j) => s + ((yprime * j.getDouble(0)) / Yprime(j)), _ + _) / topYPrimeIndices.size >= 0.5) 1 else 0
+    //if (topYPrimeIndices.aggregate(0.0)((s, row) => s + ((yprime * row._2) / Yprime.filter($"_1".===(lit(row._1))).head()._2), _+_)  / topYPrimeIndices.size >= 0.5) 1 else 0
 
 
   }
 
 
+}
+
+case class YPrime(count:Int, yre:Double, yprime:Double){
+  def getYPrime(yp:Double): Double = if(count==1) ((yp * yre) / yprime) else yprime
 }
--- multi_dimension_from_file/Util.scala
+++ multi_dimension_from_file/Util.scala
@@ -9,45 +9,32 @@ object Util {
 
   //parameters: must be fine tuned using training data
   val M=119
-  val as: List[Double] = (1 to M).map(i=>Math.pow(i.toDouble,3)).toList
+  val as: List[Double] = (1 to M).map(i=>Math.pow(i.toDouble,3)+i).toList
   val b = M+1.0
   val k = 10
 
 
 
 
-  case class Coord(x: (Row, Double)) {
-    def dist(c: Coord): Double = Math.abs(x._2 - c.x._2)
-  }
-
-  class CoordOrdering(x: Coord) extends Ordering[Coord] {
-    def compare(a: Coord, b: Coord): Int = a.dist(x) compare b.dist(x)
-  }
-
-
-  def top[T](xs: Seq[T], n: Int)(implicit ord: Ordering[T]): Seq[T] = {
-
-    def insert[T](xs: Seq[T], e: T)(implicit ord: Ordering[T]): Seq[T] = {
-      val (l, r) = xs.span(x => ord.lt(x, e))
-      (l ++ (e +: r)).take(n)
-    }
-
-    xs.drop(n).foldLeft(xs.take(n).sorted)(insert)
-  }
-
-
-
-  def prime(X: Dataset[Row], yprime : Map[Row, Double]): Map[Row, Double] = {
-    if(X.isEmpty) return yprime
-
-    val head = X.head()
-
-
-    //System.out.println("a row : "+head.mkString(", ")+" & "+head.getAs[org.apache.spark.ml.linalg.SparseVector](1).indices.mkString(", ")+" "+head.getAs[org.apache.spark.ml.linalg.SparseVector](1).values.mkString(", "))
-
-    prime(X.filter(_!=head), yprime+(head->(head.getAs[org.apache.spark.ml.linalg.SparseVector](1).values.indices.aggregate(0.0)((s, i)=>s+head.getAs[org.apache.spark.ml.linalg.SparseVector](1).values(i)*as(head.getAs[org.apache.spark.ml.linalg.SparseVector](1).indices(i)), _+_)+b)))
-
-  }
+//  case class Coord(x: Row) {
+//    def dist(c: Coord): Double = Math.abs(x.getDouble(1) - c.x.getDouble(1))
+//  }
+//
+//  class CoordOrdering(x: Coord) extends Ordering[Coord] {
+//    def compare(a: Coord, b: Coord): Int = a.dist(x) compare b.dist(x)
+//  }
+//
+//
+//  def top[T](xs: Seq[T], n: Int)(implicit ord: Ordering[T]): Seq[T] = {
+//
+//    def insert[T](xs: Seq[T], e: T)(implicit ord: Ordering[T]): Seq[T] = {
+//      val (l, r) = xs.span(x => ord.lt(x, e))
+//      (l ++ (e +: r)).take(n)
+//    }
+//
+//    xs.drop(n).foldLeft(xs.take(n).sorted)(insert)
+//  }
+//
 
   def prime(x: Row): Double = x.getAs[org.apache.spark.ml.linalg.SparseVector](1).values.indices.aggregate(0.0)((s, i)=>s+x.getAs[org.apache.spark.ml.linalg.SparseVector](1).values(i)*as(x.getAs[org.apache.spark.ml.linalg.SparseVector](1).indices(i)), _+_)+b
 
